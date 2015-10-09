384f2de8eae6b9421dac8fecf70b0d209a2d8cd4
Bryan Tong <contact@nullivex.com>
2015-10-09 11:50:11 -0600

    Add sha256 for testing

--- test/helpers/content.js
+++ test/helpers/content.js
@@ -12,7 +12,9 @@ module.exports = {
   type: 'text/plain',
   ext: 'txt',
   sha1: 'a03f181dc7dedcfb577511149b8844711efdb04f',
+  sha256: 'c11e154a748960b1777bdf3932cca9b0c8611f077ac3298c51696325c6b649ee',
   sha1Bogus: 'a03f181dc7dedcfb577511149b8844711efdb04e',
+  sha256Bogus: 'c11e154a748960b1777bdf3932cca9b0c8611f077ac3298c51696325c6b649ef',
   relativePath:
     '/a0/3f/18/1d/c7/de/dc/fb/57/75/11/14/9b/88/44/71/1e/fd/b0/4f.txt'
 }
