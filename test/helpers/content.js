5ff108d7547f681324d2857325bfb6e0b976a58f
Bryan Tong <contact@nullivex.com>
2015-10-09 11:53:59 -0600

    Update content.js

--- test/helpers/content.js
+++ test/helpers/content.js
@@ -12,9 +12,9 @@ module.exports = {
   type: 'text/plain',
   ext: 'txt',
   sha1: 'a03f181dc7dedcfb577511149b8844711efdb04f',
-  sha256: 'c11e154a748960b1777bdf3932cca9b0c8611f077ac3298c51696325c6b649ee',
+  sha256: 'a51d8fdedb2f39b4dd7fad71065ac7ef9f1a86724b4eab20f5f0106d8635fa43',
   sha1Bogus: 'a03f181dc7dedcfb577511149b8844711efdb04e',
-  sha256Bogus: 'c11e154a748960b1777bdf3932cca9b0c8611f077ac3298c51696325c6b649ef',
+  sha256Bogus: 'a51d8fdedb2f39b4dd7fad71065ac7ef9f1a86724b4eab20f5f0106d8635fa44',
   relativePath:
     '/a0/3f/18/1d/c7/de/dc/fb/57/75/11/14/9b/88/44/71/1e/fd/b0/4f.txt'
 }
